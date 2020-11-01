36b85cbe8c8f1cbe7e84371eaa6ea447428f574c
izabelazwara <izabela.z6200@gmail.com>
2020-11-01 21:42:16 +0100

    cw1

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace Cw1
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var httpClient= new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+            string responseBody = await response.Content.ReadAsStringAsync();
+            MatchCollection matches = Regex.Matches(responseBody, @"\w+@\w+.\w+");
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
