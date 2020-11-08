ce71fb1f456ef5fdda252df6fa82ddef3c18e01f
izabelazwara <izabela.z6200@gmail.com>
2020-11-08 20:20:58 +0100

    zadanie4

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -8,14 +8,45 @@ namespace Cw1
     {
         static async System.Threading.Tasks.Task Main(string[] args)
         {
+            int licznik = 0;
             var httpClient= new HttpClient();
-            var response = await httpClient.GetAsync(args[0]);
-            string responseBody = await response.Content.ReadAsStringAsync();
-            MatchCollection matches = Regex.Matches(responseBody, @"\w+@\w+.\w+");
-            foreach (Match match in matches)
+            var response = await httpClient.GetAsync("localhost");
+            try
             {
-                Console.WriteLine(match);
+                httpClient = new HttpClient();
+                response = await httpClient.GetAsync("localhost");
             }
+            catch (Exception error)
+            {
+                Console.Write("Bład w czasie pobierania strony.");
+            }
+            if (response == null)
+            {
+                throw new ArgumentNullException("Nie została podana żadna strona");
+            }
+            else
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    MatchCollection matches = Regex.Matches(responseBody, @"\w+@\w+.\w+");
+                    foreach (Match match in matches)
+                    {
+                        Console.WriteLine(match);
+                        licznik++;
+                    }
+                    if (licznik == 0)
+                    {
+                        Console.WriteLine("Nie znaleziono adresów email.");
+                    }
+                    
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+                       
         }
     }
 }
